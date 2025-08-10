794d5423caeb6e6c174b12cd969e26b321a7d2d9
mariam mansour <mariummansour19@gmail.com>
2025-08-10 13:28:18 +0200

    searchForm inhancements

--- src/assets/wrappers/SearchForm.js
+++ src/assets/wrappers/SearchForm.js
@@ -1,19 +1,63 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+
   margin-bottom: 6rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   .form {
-    display: grid;
-    grid-template-columns: 1fr auto;
+    width: 5rem;
+    height: 5rem;
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+    background: var(--white);
+    border-radius: 50%;
+    box-shadow: var(--shadow-2);
+    padding: 1rem 2rem;
+    transition: all 0.5s ease;
+    overflow: hidden;
+    box-shadow: var(--shadow-2);
   }
+
+  .form:hover {
+    width: 80%;
+    border-radius: 30px;
+    padding: 10px 20px;
+    justify-content: space-between;
+
+  }
+
   .form-input {
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
+    display: none;
+    opacity: 0;
+    outline: none;
+    border: none;
+    font-weight: 500;
+    background: transparent;
+    transition: all 0.5s ease;
   }
+
+  .form:hover .form-input {
+    display: block;
+    width: 400px;
+    opacity: 1;
+  }
+
   .btn {
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
+    background: none;
+    border: none;
+    color: var(--primary-600);
+    font-size: 1.5rem;
+    transition: var(--transition);
+    white-space: nowrap;
+    box-shadow: none;
+    align-self: center;
   }
+
 `;
 
 export default Wrapper;
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,6 +1,8 @@
 import { Form } from "react-router-dom"
 import Wrapper from "../assets/wrappers/SearchForm"
 import { useNavigation } from "react-router-dom"
+import { FaSearch } from "react-icons/fa"
+import { CgSearchLoading } from "react-icons/cg";
 
 const SearchForm = ({ searchQuery }) => {
     const navigation = useNavigation();
@@ -9,9 +11,10 @@ const SearchForm = ({ searchQuery }) => {
     return (
         <Wrapper>
             <Form className='form'>
-                <input type="search" name="search" className='form-input' placeholder='Search Cocktails...' defaultValue={searchQuery}>
+                {/* <FaSearch className="search-icon" /> */}
+                <input name="search" className='form-input' placeholder='Search Cocktails...' defaultValue={searchQuery} autoComplete="off">
                 </input>
-                <button type="submit" className='btn' disabled={isSubmitting}>{isSubmitting ? 'Searching' : 'Search'}</button>
+                <button type="submit" className='btn' disabled={isSubmitting}>{isSubmitting ? <CgSearchLoading className="search-icon" style={{fontSize: '2rem'}}/> : <FaSearch className="search-icon" />}</button>
             </Form>
         </Wrapper>
     )
